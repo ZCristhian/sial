0473dbd279e3ec5f54cdfb2e58428fb74a50ce59
Cristhian <titomiriam89@gmail.com>
2020-10-08 12:47:55 -0500

    navegacion de submenus terminado icluido responsive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const selectElement = (element) => document.querySelector(element);
+const body = document.getElementById('body')
 
 selectElement('.mobile-menu').addEventListener('click', () => {
     selectElement('header').classList.toggle('active');
+    body.classList.toggle('body-hidden')
 });
 
+//Compatibildad con Firefox
 var nameNavigator = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-
 if(nameNavigator){
     document.getElementById('header').style.backgroundColor = '#000';
 }
